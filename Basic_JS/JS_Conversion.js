406356d73acfc6353ddc1ad35e4a5440387dc62e
IbrahimSayyad <115477505+sayyad173@users.noreply.github.com>
2023-09-26 09:16:52 +0000

    Conversion of datatype is Done

---
+++ Basic_JS/JS_Conversion.js
@@ -0,0 +1,29 @@
+let score = null;
+// console.log(score);
+
+// console.log(typeof score);
+
+// #  Any value Conver To Number 
+let ValueInNumber = Number(score);
+// console.log(ValueInNumber);
+// console.log(typeof ValueInNumber);
+
+// # Conver In Boolen 
+
+let  boolen = 1;
+// console.log(boolen);
+// console.log(typeof boolen);
+
+//   Conver In Boolen method
+let ValueInBoolen = Boolean(boolen);
+// console.log(ValueInBoolen);
+
+// # Conver In String 
+let someNumber = 33;
+console.log(someNumber);
+console.log(typeof someNumber);
+
+// conver In String  method
+ let stringNumber =String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
